33af121cede4609e297e57bc593c322b3d12d241
Freddy <Freddypimpns@gmail.com>
2020-04-29 02:01:37 -0400

    probando

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-require 'vendor/autoload.php';
-
-$app = new \Slim\Slim();
-$app->POST('/foo', function() {
-    echo "I respond to multiple HTTP methods!";
-});
-$app->run();
+echo "hola";
 ?>
